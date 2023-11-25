996ac5eb3b6da9a6713af7c1260c9cdbc354e823
Harry Arinze <harryarinze@harrys-mbp.mynetworksettings.com>
2023-11-25 07:30:25 -0500

    remove install aws cli

--- vars/installawscli.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-    sh """
-     curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
-     sudo unzip awscliv2.zip
-     sudo ./aws/install
-    """
-}
